e908205fbd83869b4ca6099c1e3fc4e30f2b0e4f
David Mutua <mutuadavi@gmail.com>
2018-01-13 11:03:54 +0100

    Getters and Setters

--- dist/Classes.js
+++ dist/Classes.js
@@ -42,4 +42,42 @@ var Employee = function () {
 var e = new Employee("Street Money");
 
 // console.log(display(e.doWork())); // Work complete
-console.log(display(e.getName())); // Street Money
+// console.log(display(e.getName())); // Street Money
+
+
+// Getters and Setters.
+// Getters, gets a value and returns it to the caller.
+// Setters, takes a Parameter which you can use to set 
+// a piece of data to an Object.
+
+var Animal = function () {
+    function Animal(name) {
+        _classCallCheck(this, Animal);
+
+        this._name = name;
+    }
+
+    _createClass(Animal, [{
+        key: "name",
+        get: function get() {
+            // NB: You can manipulate the returned value.
+            return this._name.toUpperCase();
+        },
+        set: function set(newValue) {
+            // NB: Validation can be done here b4 setting.
+            this._name = newValue;
+        }
+    }]);
+
+    return Animal;
+}();
+
+var anim = new Animal("Dog");
+
+// get the name.
+console.log(anim.name); // DOG.
+
+
+// set a new name value on the name property.
+anim.name = "Buffy";
+console.log(anim.name); // BUFFY.
--- src/Classes.js
+++ src/Classes.js
@@ -28,5 +28,39 @@ class Employee {
 var e = new Employee("Street Money");
 
 // console.log(display(e.doWork())); // Work complete
-console.log(display(e.getName())); // Street Money
+// console.log(display(e.getName())); // Street Money
 
+
+
+
+
+
+// Getters and Setters.
+// Getters, gets a value and returns it to the caller.
+// Setters, takes a Parameter which you can use to set 
+// a piece of data to an Object.
+class Animal {
+    constructor (name) {
+        this._name = name;
+    }
+    
+    get name() {
+        // NB: You can manipulate the returned value.
+        return this._name.toUpperCase();
+    }
+    
+    set name(newValue) {
+        // NB: Validation can be done here b4 setting.
+        this._name = newValue;
+    }
+}
+
+var anim = new Animal("Dog");
+
+// get the name.
+console.log(anim.name); // DOG.
+
+
+// set a new name value on the name property.
+anim.name = "Buffy";
+console.log(anim.name); // BUFFY.
