075d114defa0701adec56d7ac84310a50bcb45ec
David Mutua <mutuadavi@gmail.com>
2018-02-13 09:46:40 +0100

    Explained Generators, Iterators and Symbol

---
+++ src/BuiltInObjects.js
@@ -0,0 +1,34 @@
+
+ function display(func) {
+     console.log(func);
+ }
+ 
+/*
+ * Numbers
+ * @Number Method.
+ */
+
+// Octal Literals.
+var octal = 0o71;
+
+// Binary Literals
+var bin = 0b1101;
+
+// Parse the Literals with Number Function
+var octalNum = Number("0o71");
+
+// Number Function as well exposes parseInt and parseFloat
+var doubleNum = Number.parseFloat("3.5"); // 3.5
+var intNum = Number.parseInt("1.5"); // 1
+
+// Detect Integers with isIntegers
+var isInt = Number.isInteger(1); // true if 1.0
+var isIntNow = Number.isInteger(1.5); // false
+
+// Indicate safe Integers with isSafeInteger
+// Largest Safe Int is 9007199254740991.
+var safeInt = Number.isSafeInteger(9007199254740991);
+
+
+var radNum = +new Date(); // radom number.
+display(radNum);
--- src/Generators.js
+++ src/Generators.js
@@ -1,3 +1,108 @@
+/*
+ *  yield, pauses a generator execution cycle.
+ *  it also returns a value.
+ */
+
+// NB: You can pause a Generator using yield the way u wish to.
+// A generator it's a state machine which pauses and remembers 
+// it's current state, then moves forward.
 
 
+// NB: A Generator will return a true only when it gets 
+// to the END( last yield ).
 
+
+// Define a Generator
+function *main() {
+    console.log("Hey");
+    yield;
+    console.log("Young");
+    yield 9;
+    console.log("World");
+    yield 10;
+}
+
+var it = main();
+
+//it.next(); // { value: undefined, done: false }
+
+//it.next(); // { value: 9, done: false }
+
+//it.next(); // { value: 10, done: true }
+
+
+// Another Generator, to yield values automatically.
+function *mygenerator () {
+    
+    // Simulate Looping Records from a DB then yield them;
+    for(let x=0; x<=5; x++){
+        yield x; // return thread of execution back to "for of" caller.
+    }
+}
+
+// now output yielded values.
+// "for of" loops thr values only but not keys or indices.
+for (let i of mygenerator()) {
+    // console.log(i); // now use the yielded value.
+}
+
+
+/*
+ *  Symbols
+ *  Declared: var x = Symbol("Optional Description");
+ */
+
+// Def: Unique Globally unguesable value within the context of your program.
+
+// NB: You will NEVER need to create your own Symbols.
+// NB: Used Opaquely i.e. Don't care of it's value.
+// NB: Two Symbols will never be EQUAL.
+
+
+// Well Known Symbols; JS Ships with Properties on the Symbol Object.
+// That is:
+
+// Symbol.iterator
+// Symbol.toStringTag
+// Symbol.toPrimitive
+// Symbol.isConcatSpreadable
+
+
+/*
+ * Iterators
+ */
+
+// Def: It's a patterned way of stepping through set of data on some datasource.
+
+// NB: Arrays ship with a Symbol.iterator property which steps through the Array 
+// one item at a time.
+// NB: The property it's a function which we can invoke asap.
+
+// @Example;
+var arr = [1, 2, 3];
+
+var it = arr[Symbol.iterator](); // returns an instance of an iterator.
+
+// But "for of" loop comes to the rescue to hasten things up.
+for(let j of arr){
+    // console.log(j);
+}
+
+// NB: Objects Do not ship with a Default iterator.
+
+
+// Custom Iterators.
+// Make an Object Iterable.
+var obj = {
+    *[Symbol.iterator]() {
+        for (var i = this.start; i < this.end; i++) {
+            yield this.values[i];
+        }
+    },
+    start: 1,
+    end: 5,
+    values: [2, 4, 6, 10, 5, 7, 9]
+};
+
+var vals = [ ...obj ];
+console.log(vals);
